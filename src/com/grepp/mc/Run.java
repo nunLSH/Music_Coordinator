2c92fd22f115ca84e0aa8255845a38bc40430c5c
imseohyeon <forestlight749@gmail.com>
2025-03-18 11:20:15 +0900

    (0318) refactor: QR Code 생성

--- src/com/grepp/mc/Run.java
+++ src/com/grepp/mc/Run.java
@@ -6,6 +6,8 @@ import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 import com.google.zxing.qrcode.decoder.QRCodeDecoderMetaData;
+import com.grepp.mc.infra.error.CommonException;
+import com.grepp.mc.infra.util.QrCodeUtil;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -19,15 +21,12 @@ public class Run {
         String name = sc.nextLine();
         System.out.print("qrcode로 만들 문자열 : ");
         String content = sc.nextLine();
-
-        QRCodeWriter writer = new QRCodeWriter();
-
         String format = "png";
+
         try (FileOutputStream fos = new FileOutputStream(name + "." + format)) {
-            BitMatrix matrix = writer.encode(content, BarcodeFormat.QR_CODE, 300, 300);
-            MatrixToImageWriter.writeToStream(matrix, format, fos);
-        } catch (WriterException | IOException e) {
-            throw new RuntimeException(e);
+            QrCodeUtil.createQrCode(format, content, fos);
+        } catch (CommonException | IOException e) {
+            e.printStackTrace();
         }
     }
 
