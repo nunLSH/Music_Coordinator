29d6e6bb13339a78a7d28bdb4d2c6778fd591dd8
imseohyeon <forestlight749@gmail.com>
2025-03-18 16:07:04 +0900

    (0318) HTTP > GeminiChatModel 따로 분리

--- src/com/grepp/mc/Run.java
+++ src/com/grepp/mc/Run.java
@@ -1,9 +1,11 @@
 package com.grepp.mc;
 
 import com.google.gson.Gson;
+import com.grepp.mc.infra.llm.GeminiChatModel;
 import com.grepp.mc.infra.llm.gemini.text.vo.Content;
 import com.grepp.mc.infra.llm.gemini.text.vo.Part;
 import com.grepp.mc.infra.llm.gemini.text.vo.RequestDocument;
+import com.grepp.mc.infra.llm.gemini.text.vo.generation.GenerationConfig;
 import java.util.List;
 import java.util.function.BiPredicate;
 
@@ -12,8 +14,9 @@ public class Run {
     public static void main(String[] args) {
 
         RequestDocument doc = new RequestDocument("오늘 저녁 추천해줘");
-        String data = new Gson().toJson(doc);
-        System.out.println(data);
+
+        new GeminiChatModel().invoke(doc);
+
 
     }
 }
