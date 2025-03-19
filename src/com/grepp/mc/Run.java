31e8fcd7301763b374fc90d408035638afdfb8a3
imseohyeon <forestlight749@gmail.com>
2025-03-19 09:52:57 +0900

    (0319) feat: QR Code 생성하여 음악 추천

--- src/com/grepp/mc/Run.java
+++ src/com/grepp/mc/Run.java
@@ -8,22 +8,25 @@ import com.grepp.mc.infra.llm.ChatModel;
 import com.grepp.mc.infra.llm.Response;
 import com.grepp.mc.infra.llm.gemini.text.GeminiChatModel;
 import com.grepp.mc.infra.llm.gemini.text.TextRequest;
+import com.grepp.mc.presentation.Index;
 import java.util.List;
 import java.util.Map;
 
 @SuppressWarnings("unchecked")
 public class Run {
     public static void main(String[] args) {
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        String msg = "심심할 때 듣기 좋은 노래 추천해줘";
+//        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+//        String msg = "심심할 때 듣기 좋은 노래 추천해줘";
+//
+//        ChatModel model = new GeminiChatModel();
+//        Response response = model.invoke(new TextRequest<>(msg, SongFormat.format));
+//        List<Map<String, Object>> data = (List<Map<String, Object>>)
+//            response.response();
+//
+//        List<Song> songs = data.stream().map(SongFormat::toVo).toList();
+//        System.out.println(songs);
 
-        ChatModel model = new GeminiChatModel();
-        Response response = model.invoke(new TextRequest<>(msg, SongFormat.format));
-        List<Map<String, Object>> data = (List<Map<String, Object>>)
-            response.response();
-
-        List<Song> songs = data.stream().map(SongFormat::toVo).toList();
-        System.out.println(songs);
+        new Index().index();
 
     }
 }
--- src/com/grepp/mc/domain/coordinator/MusicCoordinator.java
+++ src/com/grepp/mc/domain/coordinator/MusicCoordinator.java
@@ -1,14 +1,28 @@
 package com.grepp.mc.domain.coordinator;
 
+import static java.util.stream.Collectors.toList;
+
+import com.grepp.mc.domain.qrcode.QrCodeGenerator;
 import com.grepp.mc.domain.song.Song;
+import com.grepp.mc.domain.song.SongFormat;
+import com.grepp.mc.infra.llm.ChatModel;
+import com.grepp.mc.infra.llm.Response;
+import com.grepp.mc.infra.llm.gemini.text.GeminiChatModel;
+import com.grepp.mc.infra.llm.gemini.text.TextRequest;
+import javax.sql.rowset.serial.SerialJavaObject;
 
 public class MusicCoordinator {
 
-    public Song coordinate(String msg){
-
+    public Song recommend(String msg) {
         msg += " . 어울리는 노래 1곡 추천해줘. 추천 이유도 알려줘.";
 
-        return null;
-    }
+        ChatModel model = new GeminiChatModel();
+        Response response = model.invoke(new TextRequest<>(msg, SongFormat.format));
 
+        Song song = response.response().stream().map(SongFormat::toVo).toList().getFirst();
+        QrCodeGenerator qrCodeGenerator = new QrCodeGenerator();
+        qrCodeGenerator.generate(song);
+
+        return response.response().stream().map(SongFormat::toVo).toList().getFirst();
+    }
 }
